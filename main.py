43fecc0d25dc488572bfafce6889fe61e60243bd
EdwardAlexanderCanalesYarin <114418185+EdwardAlexanderCanalesYarin@users.noreply.github.com>
2024-05-02 20:55:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tempfile
 import os
 from flask import Flask, request, redirect, send_file
 from skimage import io
+from skimage.transform import resize
 import base64
 import glob
 import numpy as np
@@ -10,101 +11,124 @@ app = Flask(__name__)
 
 main_html = """
 <html>
-<head></head>
-<script>
-  var mousePressed = false;
-  var lastX, lastY;
-  var ctx;
+	<head>
+		<link rel="stylesheet" type="text/css" href="static/styles.css">
+	</head>
+	<script>
+	  var mousePressed = false;
+	  var lastX, lastY;
+	  var ctx;
 
-   function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-   }
+	   function getRndInteger(min, max) {
+	    return Math.floor(Math.random() * (max - min) ) + min;
+	   }
 
-  function InitThis() {
-      ctx = document.getElementById('myCanvas').getContext("2d");
-
-
-      numero = getRndInteger(0, 10);
-      letra = ["U", "N", "I"];
-      random = Math.floor(Math.random() * letra.length);
-      aleatorio = letra[random];
-
-      document.getElementById('mensaje').innerHTML  = 'Dibujando un ' + aleatorio;
-      document.getElementById('numero').value = aleatorio;
-
-      $('#myCanvas').mousedown(function (e) {
-          mousePressed = true;
-          Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-      });
-
-      $('#myCanvas').mousemove(function (e) {
-          if (mousePressed) {
-              Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-          }
-      });
-
-      $('#myCanvas').mouseup(function (e) {
-          mousePressed = false;
-      });
-  	    $('#myCanvas').mouseleave(function (e) {
-          mousePressed = false;
-      });
-  }
-
-  function Draw(x, y, isDown) {
-      if (isDown) {
-          ctx.beginPath();
-          ctx.strokeStyle = 'black';
-          ctx.lineWidth = 11;
-          ctx.lineJoin = "round";
-          ctx.moveTo(lastX, lastY);
-          ctx.lineTo(x, y);
-          ctx.closePath();
-          ctx.stroke();
-      }
-      lastX = x; lastY = y;
-  }
-
-  function clearArea() {
-      // Use the identity matrix while clearing the canvas
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  }
-
-  //https://www.askingbox.com/tutorial/send-html5-canvas-as-image-to-server
-  function prepareImg() {
-     var canvas = document.getElementById('myCanvas');
-     document.getElementById('myImage').value = canvas.toDataURL();
-  }
+	  function InitThis() {
+	      ctx = document.getElementById('myCanvas').getContext("2d");
 
 
+	      numero = getRndInteger(0, 10);
+	      letra = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+	      random = Math.floor(Math.random() * letra.length);
+	      aleatorio = letra[random];
 
-</script>
-<body onload="InitThis();">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" ></script>
-    <div align="left">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Uni-logo_transparente_granate.png" width="300"/>
-    </div>
-    <div align="center">
-        <h1 id="mensaje">Dibujando...</h1>
-        <canvas id="myCanvas" width="200" height="200" style="border:2px solid black"></canvas>
-        <br/>
-        <br/>
-        <button onclick="javascript:clearArea();return false;">Borrar</button>
-    </div>
-    <div align="center">
-      <form method="post" action="upload" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
-      <input id="numero" name="numero" type="hidden" value="">
-      <input id="myImage" name="myImage" type="hidden" value="">
-      <input id="bt_upload" type="submit" value="Enviar">
-      </form>
-    </div>
-</body>
+	      document.getElementById('mensaje').innerHTML  = 'Escriba en kanji el dia ' + aleatorio;
+	      document.getElementById('numero').value = aleatorio;
+
+	      $('#myCanvas').mousedown(function (e) {
+		  mousePressed = true;
+		  Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+	      });
+
+	      $('#myCanvas').mousemove(function (e) {
+		  if (mousePressed) {
+		      Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+		  }
+	      });
+
+	      $('#myCanvas').mouseup(function (e) {
+		  mousePressed = false;
+	      });
+	  	    $('#myCanvas').mouseleave(function (e) {
+		  mousePressed = false;
+	      });
+	  }
+
+	  function Draw(x, y, isDown) {
+	      if (isDown) {
+		  ctx.beginPath();
+		  ctx.strokeStyle = 'black';
+		  ctx.lineWidth = 11;
+		  ctx.lineJoin = "round";
+		  ctx.moveTo(lastX, lastY);
+		  ctx.lineTo(x, y);
+		  ctx.closePath();
+		  ctx.stroke();
+	      }
+	      lastX = x; lastY = y;
+	  }
+
+	  function clearArea() {
+	      ctx.setTransform(1, 0, 0, 1, 0, 0);
+	      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	  }
+
+	  //https://www.askingbox.com/tutorial/send-html5-canvas-as-image-to-server
+	  function prepareImg() {
+	     var canvas = document.getElementById('myCanvas');
+	     document.getElementById('myImage').value = canvas.toDataURL();
+	  }
+
+
+
+	</script>
+	
+	<body onload="InitThis();">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+	    <script type="text/javascript" ></script>
+	    <div align="center">
+		<h1 id="mensaje">Dibujando...</h1>
+		<canvas id="myCanvas" width="200" height="200" style="border:2px solid black"></canvas>
+		<br/>
+		<br/>
+		<div id="botones1">
+		      <button onclick="javascript:clearArea();return false;">Borrar</button>
+		      <form method="post" action="upload" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+			      <input id="numero" name="numero" type="hidden" value="">
+			      <input id="myImage" name="myImage" type="hidden" value="">
+			      <input id="bt_upload" type="submit" value="Enviar">
+		      </form>
+	    	</div>
+	    	
+		<div id="botones2">
+			<form method="get" action="prepare" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+			      <input id="numero" name="numero" type="hidden" value="">
+			      <input id="myImage" name="myImage" type="hidden" value="">
+			      <input id="bt_upload" type="submit" value="prepara datos">
+		      </form>
+		      <form method="get" action="X.npy" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+			      <input id="numero" name="numero" type="hidden" value="">
+			      <input id="myImage" name="myImage" type="hidden" value="">
+			      <input id="bt_upload" type="submit" value="descargar x">
+		      </form>
+		      <form method="get" action="y.npy" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+			      <input id="numero" name="numero" type="hidden" value="">
+			      <input id="myImage" name="myImage" type="hidden" value="">
+			      <input id="bt_upload" type="submit" value="decargar y">
+		      </form>
+		</div>
+		
+		
+		
+	    </div>
+	    
+	    <div id="imagen" align="center">
+      		<img src="https://marcjapan.files.wordpress.com/2007/02/kanji2x.JPG" width="300"/>
+    	    </div>
+	</body>
 </html>
 
-"""
-
+""" 
 @app.route("/")
 def main():
     return(main_html)
@@ -112,25 +136,23 @@ def main():
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
-        # check if the post request has the file part
-        img_data = request.form.get('myImage').replace("data:image/png;base64,","")
+        img_data = request.form.get('myImage').replace("data:image/png;base64,", "")
         aleatorio = request.form.get('numero')
-        print(aleatorio)
+        folder_id = '1SAexmaIUn4_SlQUiPLWjDuZcZiSiMuzy'  # Reemplaza con la ID de tu carpeta en Google Drive
+        file_name = aleatorio + '_image.png'
         with tempfile.NamedTemporaryFile(delete = False, mode = "w+b", suffix='.png', dir=str(aleatorio)) as fh:
             fh.write(base64.b64decode(img_data))
-        #file = request.files['myImage']
-        print("Image uploaded")
+        print(f"Image uploaded {file_name}")
     except Exception as err:
         print("Error occurred")
         print(err)
 
     return redirect("/", code=302)
-
-
+    
 @app.route('/prepare', methods=['GET'])
 def prepare_dataset():
     images = []
-    d = ["I","N","U"]
+    d = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
     digits = []
     for digit in d:
       filelist = glob.glob('{}/*.png'.format(digit))
@@ -143,7 +165,7 @@ def prepare_dataset():
     digits = np.concatenate(digits)
     np.save('X.npy', images)
     np.save('y.npy', digits)
-    return "OK!"
+    return "EL DATA SET HA SIDO PROCESADO :3"
 
 @app.route('/X.npy', methods=['GET'])
 def download_X():
@@ -151,9 +173,10 @@ def download_X():
 @app.route('/y.npy', methods=['GET'])
 def download_y():
     return send_file('./y.npy')
-
+    
 if __name__ == "__main__":
-    digits = ['U', 'N', 'I']
+    
+    digits = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
     for d in digits:
         if not os.path.exists(str(d)):
             os.mkdir(str(d))
